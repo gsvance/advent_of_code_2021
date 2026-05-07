5e1ce4ddd6565ad1bd5bc37218484c5f2c42eaea
Gregory Vance <13666175+gsvance@users.noreply.github.com>
2026-05-07 15:30:10 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Advent of Code 2021
+
+My solutions to Advent of Code 2021 in Python
+
+- I promised myself that I would not start working on solving problems from AoC 2021 until I finished up both AoC 2023 and AoC 2024.
+  - 2023 -- DONE
+  - 2024 -- NOT YET
---
+++ start_next_problem.py
@@ -0,0 +1,57 @@
+from pathlib import Path
+from typing import Final
+
+
+NEW_SCRIPT_CONTENTS: Final[str] = '''
+
+
+import pathlib
+import sys
+
+
+def part_1(file: pathlib.Path) -> None:
+    file.read_text(encoding='ascii')
+    print('part 1:', )
+
+
+def part_2(file: pathlib.Path) -> None:
+    file.read_text(encoding='ascii')
+    print('part 2:', )
+
+
+if __name__ == '__main__':
+    _, arg_1 = sys.argv
+    arg_1_path = pathlib.Path(arg_1)
+    part_1(arg_1_path)
+    part_2(arg_1_path)
+
+
+'''.strip() + '\n'
+
+
+def main() -> None:
+    existing_folders = [
+        item for item in Path('.').glob('dec_*') if item.is_dir()]
+    existing_numbers = [
+        int(folder.name.removeprefix('dec_')) for folder in existing_folders
+    ]
+    assert set(existing_numbers) == set(range(1, len(existing_numbers) + 1))
+
+    next_number = len(existing_numbers) + 1
+    if next_number > 25:
+        print('Error: there are already 25 problem folders')
+        return
+
+    next_folder = Path('.', f'dec_{next_number:02d}')
+    next_script = next_folder / f'{next_folder.name}.py'
+    next_example_1 = next_folder / 'example_1.txt'
+    next_input = next_folder / 'input.txt'
+
+    next_folder.mkdir()
+    next_script.write_text(NEW_SCRIPT_CONTENTS)
+    next_example_1.write_text('')
+    next_input.write_text('')
+
+
+if __name__ == '__main__':
+    main()
